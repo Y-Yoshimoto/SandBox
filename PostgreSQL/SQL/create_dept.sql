8db858c7ab3c1681ef569cab23f5c7c24cf6e718
Y-Yoshimoto <mewyoshimoto@gmail.com>
2016-11-17 22:52:53 +0900

    テスト用テーブル作成のSQLファイルを追加

---
+++ PostgreSQL/SQL/create_dept.sql
@@ -0,0 +1,12 @@
+create table dept (
+    dept_id int,
+    dept_name VARCHAR(255),
+    dept_area VARCHAR(255)
+);
+
+insert into dept values
+    (10,"system","tokyo"),
+    (20,"system","osaka"),
+    (30,"sales","LA"),
+    (40,"edu","tokyo"),
+    (50,"develop","nagoya");
