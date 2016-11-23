e4c17458fea70249e6440fb1732af13496e84228
Y-Yoshimoto <mewyoshimoto@gmail.com>
2016-11-23 13:42:53 +0900

    テーブル追加

--- PostgreSQL/SQL/create_dept.sql
+++ PostgreSQL/SQL/create_dept.sql
@@ -5,8 +5,20 @@ create table dept (
 );
 
 insert into dept (dept_id, dept_name, dept_area) values
-    (10,'system','tokyo'),
-    (20,'system','osaka'),
-    (30,'sales','LA'),
-    (40,'edu','tokyo'),
-    (50,'develop','nagoya');
+    (10,"system","tokyo"),
+    (20,"system","osaka"),
+    (30,"sales","LA"),
+    (40,"edu","tokyo"),
+    (50,"develop","nagoya");
+
+Create table emp(
+    emp_id int,
+    emp_name VARCHAR(255),
+    dept_id int
+    );
+
+insert into emp values
+    (101,"sato keiji",10),
+    (102,"suzuki kenichi",40),
+    (103,"mikami hideki",40),
+    (104,"aoki tomio",20);
