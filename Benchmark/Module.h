ca964f49f4114586f0431d88cb423d3ec7f03328
Y-Yoshimoto <mewyoshimoto@gmail.com>
2016-12-09 01:55:44 +0900

    new file:   Benchmark/Hist.plot  	new file:   Benchmark/Integration.h  	new file:   Benchmark/Module.h  	new file:   Benchmark/Ran.sh  	new file:   Benchmark/main.c

---
+++ Benchmark/Module.h
@@ -0,0 +1,25 @@
+//
+//  Module.h
+//  P4_1
+//
+//  Created by 吉本雄介 on 2015/07/01.
+//  Copyright (c) 2015年 吉本雄介. All rights reserved.
+//
+
+#ifndef P4_1_Module_h
+#define P4_1_Module_h
+
+#define THREADS 1
+#define HSize 2000
+#define PPI 2.0*M_PI
+#define Bin 120000000
+//#define Try 100000
+#define Low -5.0
+#define High 5.0
+double Hset=(double)HSize/(High-Low);
+int Array_size=2*Bin;
+//int Array_size=6*Bin;
+
+
+
+#endif
