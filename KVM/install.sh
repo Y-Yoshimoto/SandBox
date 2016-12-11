93ec772fb18cd0c83d77e57ecea56439a16b38af
Y-Yoshimoto <mewyoshimoto@gmail.com>
2016-12-11 22:45:50 +0900

    インストール成功

--- KVM/install.sh
+++ KVM/install.sh
@@ -1,11 +1,12 @@
+#qemu-img create -f qcow2 WebAP1.qcow2 5G
 virt-install \
 --name WebAP1 \
---ram 640 \
---disk path=/media/y-yoshimoto/KVM128G/WebAP1.img,size=20G,format=raw\
+--ram=640 \
+--disk path='/media/y-yoshimoto/KVM128G/webap1.qcow2',size=10,format=qcow2 \
 --vcpus 1 \
 --os-type linux \
 --os-variant rhel7 \
---network bridge=br0 \
+--network network=default \
 --graphics none \
 --console pty,target_type=serial \
 --location '/home/y-yoshimoto/Downloads/CentOS-7-x86_64-Minimal-1511.iso' \
