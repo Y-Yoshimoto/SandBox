d0212cd4a2d63da723e941a5c79974bd91db03c6
Y-Yoshimoto <mewyoshimoto@gmail.com>
2017-04-01 00:47:33 +0900

    modified:   install.sh

--- KVM/install.sh
+++ KVM/install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-#qemu-img create -f qcow2 WebAP1.qcow2 5G
+# --name, --diskを編集
+#qemu-img create -f qcow2 server.qcow2 5G
 virt-install \
---name WebAP1 \
+--name server \
 --ram=640 \
---disk path='/media/y-yoshimoto/KVM128G/webap1.qcow2',size=10,format=qcow2 \
+--disk path='/media/y-yoshimoto/KVM128G/server.qcow2',size=10,format=qcow2 \
 --vcpus 1 \
 --os-type linux \
 --os-variant rhel7 \
 --network network=default \
 --graphics none \
 --console pty,target_type=serial \
---location '/home/y-yoshimoto/Downloads/CentOS-7-x86_64-Minimal-1511.iso' \
+--location '/home/y-yoshimoto/Downloads/CentOS-7-x86_64-Minimal-1611.iso' \
 --extra-args 'console=ttyS0,115200n8 serial'
