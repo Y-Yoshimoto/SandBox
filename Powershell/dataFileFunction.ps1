d90218066f6d39d5479efcd2e7c1e3f8091c068c
y-yoshimoto <mewyoshimoto@gmail.com>
2020-08-24 23:54:04 +0900

    ファイルIO用の関数の雛形を準備

---
+++ Powershell/dataFileFunction.ps1
@@ -0,0 +1,24 @@
+# csv,json データファイルの読み書き関数
+
+## Sampledata
+## https://www.post.japanpost.jp/zipcode/dl/jigyosyo/index-zip.html
+### 
+function Get-SampleData(){
+
+}
+
+function Load-csvToObject(){
+
+}
+
+function Save-csvFromObject(){
+
+}
+
+function Save-jsonFromObject(){
+
+}
+
+function Convert-ObjectToHashtables(){
+    
+}
